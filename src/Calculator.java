2ca292b61ad12154b2f7f5a978c879ec865cc017
conglp90 <conglp90@gmail.com>
2013-06-04 14:49:55 +0700

    step 8: Red

---
+++ src/Calculator.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+
+public class Calculator {
+	ArrayList<Integer> sohang_list=new ArrayList<Integer>();
+	String delimitter="";
+	
+	
+	void process_laySoHang(String input){// xu ly xau co dang 1,2,3,4
+		for (String temp :input.split(","))
+			sohang_list.add(Integer.parseInt(temp));
+	}
+	
+	private String process_delimiter(String input) {
+		String kq;
+		if (input.contains("//")) {
+			delimitter=input.substring(input.indexOf("[")+1,input.indexOf("]"));
+			input=input.substring(input.indexOf("\n")+1);
+			input=input.replaceAll(delimitter,",");
+		} 
+		kq=input.replaceAll("\n",",");
+		return kq;
+	}
+	
+	int add(String input){
+		int kq=0;
+		try {
+			process_laySoHang( process_delimiter(input));			
+		} catch (Exception e) {
+			// TODO: handle exception
+			return 0;
+		}
+		
+		process_negatives(sohang_list);
+		
+		for(int sohang: sohang_list){
+			kq+=sohang%1000; // 1003 <-> 3
+			System.out.println("abc: "+sohang);
+		}
+		return kq;
+	}
+
+	private void process_negatives(ArrayList<Integer> sohang_list2) {
+		String messendger="negatives not allowed: ";
+		boolean isHasNegatives=false;
+		for(int sohang: sohang_list){
+			if (sohang<0) {
+				if(isHasNegatives)
+					messendger+=", "; 
+				messendger+= sohang;				
+				isHasNegatives=true;
+			}
+		}	
+		if(isHasNegatives)
+			throw new RuntimeException(messendger);
+	
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
